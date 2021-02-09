ea0c5a1d9fd99deee1c9d900090922ba23e62ee1
Mila Bromm <43033329+NotARectangle@users.noreply.github.com>
2021-02-09 20:37:05 +0000

    Tokenizer stage complete for test

--- Datatest/GPT2test1.py
+++ Datatest/GPT2test1.py
@@ -7,10 +7,11 @@ data = json.load(open('../Dataset/AllPicardLines.json', 'r'))
 
 #print(data.keys())
 def build_text_files(data_json, dest_path):
-     f = open(dest_path, 'w')
+     f = open(dest_path, 'w', encoding='utf-8')
      data = ''
      for texts in data_json:
-         data += texts.encode('utf-8').strip() + " "
+         summary = str(texts).strip()
+         data += summary + " "
      f.write(data)
 
 train, test = train_test_split(data['PICARD'],test_size=0.15)
---
+++ Datatest/PicardLinesWriter.py
@@ -0,0 +1,22 @@
+import json
+
+character = "PICARD"
+series = "TNG"
+
+
+data = json.load(open('../Dataset/all_series_lines.json', 'r'))
+
+character_lines = []
+all_lines = {}
+all_lines[character] = []
+tng_episodes = data[series].keys()
+for ep in tng_episodes:
+#    print(ep)
+    script = data[series][ep]
+    character_lines = script[character]
+
+    for l in character_lines:
+        all_lines[character].append(l)
+
+with open('../Dataset/AllPicardLines.json', 'w') as json_file:
+  json.dump(all_lines, json_file)
