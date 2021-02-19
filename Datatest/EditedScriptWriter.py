995a96c16d03914586f9c1e1e5cedb3445a9543e
NotARectangle <milenabromm@hotmail.de>
2021-02-19 12:51:56 +0000

    EditedVersion of full script

--- Datatest/EditedScriptWriter.py
+++ Datatest/EditedScriptWriter.py
@@ -9,46 +9,51 @@ firstEpisode = data['episode 0']
 #take out stage direction (between brackets)
 
 #Take out ending "<back"
+episodes = data.keys()
+newData = {}
 
-firstEpisode = firstEpisode[0: firstEpisode.index("<Back")].strip()
+for ep in episodes:
+    EpisodeScript = data[ep]
+    EpisodeScript = EpisodeScript[0: EpisodeScript.index("<Back")].strip()
+    EpisodeScript = EpisodeScript.replace("\n", " ")
 
-firstEpisode = firstEpisode.replace("\n", " ")
-#leav out episode name? start with captains log.. Episode name probably not important data
-captainsLog = firstEpisode[firstEpisode.index("Captain's log"):firstEpisode.index("[")].strip()
+    #get all captains Logs
+    captainRe = "(Captain's log[^\[]+)"
+    AllLogs = []
+    AllLogs= re.findall(captainRe, EpisodeScript)
+    index = 0
+    #remove all captains logs
+    #firstEpisode = firstEpisode.replace("Captain's log[^\[]+", " ")
+    #strip trailing whitespace
+    while index < len(AllLogs):
+        EpisodeScript = EpisodeScript.replace(AllLogs[index], " ")
+        #make all whitespace instances just one space
+        AllLogs[index] = re.sub(r"\s+", " ", AllLogs[index])
+        AllLogs[index] = AllLogs[index].strip()
+        index += 1
 
-#get all captains Logs
-captainRe = "(Captain's log[^\[]+)"
-AllLogs= re.findall(captainRe, firstEpisode)
-index = 0
-#remove all captains logs
-#firstEpisode = firstEpisode.replace("Captain's log[^\[]+", " ")
-#strip trailing whitespace
-while index < len(AllLogs):
-    firstEpisode = firstEpisode.replace(AllLogs[index], " ")
-    #make all whitespace instances just one space
-    AllLogs[index] = re.sub(r"\s+", " ", AllLogs[index])
-    AllLogs[index] = AllLogs[index].strip()
-    index += 1
-#print(AllLogs)
 
-#find scenes
-settingRegex = "\[[\w\s']+\](?!\:)"
 
-#split scenes by setting notation ex. [Bridge]
-scenes = firstEpisode[firstEpisode.index("["):]
-x = re.split(settingRegex, scenes)
-x.remove("")
 
-index = 0
-#delete trailing whitespace
-while index < len(x):
-    #delete  [OC]-overcomms notation
-    x[index] = re.sub(r"\[[\w\s']+\]", "", x[index].strip())
-    #make all whitespace instances just one space
-    x[index] = re.sub(r"\s+", " ", x[index])
-    index += 1
+    #find scenes
+    settingRegex = "\[[\w\s'-]+\](?!\:)"
+    x = []
+    #split scenes by setting notation ex. [Bridge]
+    scenes = EpisodeScript[EpisodeScript.index("["):]
+    x = re.split(settingRegex, scenes)
+    #x.remove("")
 
-dict = {"Logs" : AllLogs, "Scenes" : x}
+    index = 0
+    #delete trailing whitespace
+    while index < len(x):
+        #delete  [OC]-overcomms notation
+        x[index] = re.sub(r"\[[\w\s']+\]", "", x[index].strip())
+        #make all whitespace instances just one space
+        x[index] = re.sub(r"\s+", " ", x[index])
+        index += 1
 
-with open('../Dataset/TNGScriptCutEp0.json', 'w', encoding='utf-8') as json_file:
-  json.dump(dict, json_file)
+    dict = {"Logs" : AllLogs, "Scenes" : x}
+    newData[ep] = dict
+
+with open('../Dataset/TNGScriptCut.json', 'w', encoding='utf-8') as json_file:
+  json.dump(newData, json_file)
